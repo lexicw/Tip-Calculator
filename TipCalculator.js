8a7733d7f26871a838ffafcf1afdb0b9d930be42
lexicw15 <lexicw15@yahoo.com>
2021-01-27 17:06:00 -0500

    Small Updates
    Made some small updates of the code

--- TipCalculator.js
+++ TipCalculator.js
@@ -1,9 +1,9 @@
 	/* Author:       Alexandra Wheeler             */
 function tipCalculate() {
   event.preventDefault();
-  var cost = document.getElementById("cost").value;
-  var p = document.getElementById("percentage").value;
-  var message;
+  let cost = document.getElementById("cost").value;
+  let p = document.getElementById("percentage").value;
+  let message;
   message = document.getElementById("errorMsg");
   message.innerHTML = "";
   
@@ -11,8 +11,8 @@ function tipCalculate() {
     if(cost == "") throw "empty";
     if(isNaN(cost)) throw "not a number";
 	else {
-	var tipCalc = cost * p; 
-	var total = parseFloat(cost) + parseFloat(tipCalc);
+	let tipCalc = cost * p; 
+	let total = parseFloat(cost) + parseFloat(tipCalc);
 	console.log("total = " + total);
 	document.getElementById("tipAmount").value = tipCalc;
 	document.getElementById("totalCost").value = total;
